ce9fdc7a883664d8b9284a9a3d6248e823dacf49
xinghunm <xinghunm@curacloudcorp.com>
2024-08-07 22:43:39 +0800

    add class and object example

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,6 @@
+// 类名必须和文件名相同
 public class HelloWorld {
+    // 所有的 Java 程序由 public static void main(String[] args) 方法开始执行。
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
